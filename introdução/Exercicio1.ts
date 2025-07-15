7b8ae45370a6ca5594c2095369adc9432b59b943
Letícia Betman <leticiarodriguesbetman@gmail.com>
2025-07-14 23:24:36 -0300

    Update Exercicio1.ts

--- introdução/Exercicio1.ts
+++ introdução/Exercicio1.ts
@@ -13,19 +13,3 @@ console.log(cores);
 
 console.log("\nCores em ordem crescente:");
 console.log(cores.sort());
-
-//Exercício 3 (Lista 02)
-const numeros = new Set<number>();
-
-while (numeros.size < 10) {
-  const valor = Number(readline.question(`Digite o ${numeros.size + 1}º numero: `));
-
-  if (numeros.has(valor)) {
-    console.log("Numero repetido! Digite outro.");
-  } else {
-    numeros.add(valor);
-  }
-}
-
-console.log("\nNumeros digitados:");
-console.log(Array.from(numeros));
